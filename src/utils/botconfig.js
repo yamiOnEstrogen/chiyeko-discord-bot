1c1ae4229e02df8390f07303b46d12d17710eea7
kiyolol <predevxyzlol@gmail.com>
2023-04-02 22:02:04 -0400

    f

--- src/utils/botconfig.js
+++ src/utils/botconfig.js
@@ -2,5 +2,5 @@ module.exports = {
     memberRole: "1091906241552465971", //? member role (../events/guildMemberAdd.js)
     welcomeChannel: "1091914863720280125", //? welcome channel (../events/guildMemberAdd.js)
     guildID: "1091905976619249724", //? guild id (GLOBAL)
-    verifyChannel: "1091914133575839867", //? verify channel (../events/messageCreate.js)
+    verifyChannel: "1092203257071411200", //? verify channel (../events/messageCreate.js)
 }
